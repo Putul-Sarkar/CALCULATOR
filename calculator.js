08cf21225ffd0fed6c8fd27c702e6c0487cc4f42
Putul-Sarkar <putulsarkar8@gmail.com>
2023-09-08 12:52:26 +0600

    calculator final

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function deleteMe() {
+    document.getElementById('myResult').value = '';
+}
+
+function calculator(newValue) {
+    document.getElementById('myResult').value
+        += newValue;
+}
+
+function answer() {
+    var a = document.getElementById('myResult').value;
+    var b = eval(a);
+    document.getElementById("myResult").value = b
+}
